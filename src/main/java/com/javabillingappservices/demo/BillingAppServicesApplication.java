5f524d277204f6a19c7541229bd31508c2c6fe49
amallaltlai <amal.lal@outlook.com>
2023-08-22 13:28:17 +0530

    Creating Java services for billing app

---
+++ src/main/java/com/javabillingappservices/demo/BillingAppServicesApplication.java
@@ -0,0 +1,14 @@
+package com.javabillingappservices.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingAppServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingAppServicesApplication.class, args);
+		System.out.println("Amal");
+	}
+
+}
---
+++ src/test/java/com/javabillingappservices/demo/BillingAppServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javabillingappservices.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingAppServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
