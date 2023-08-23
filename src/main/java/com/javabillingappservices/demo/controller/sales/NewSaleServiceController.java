657903a26d38e40d8ed66078f69cd35fb23dad5a
amallaltlai <amal.lal@outlook.com>
2023-08-23 21:11:36 +0530

    Basic new sale post Request

--- src/main/java/com/javabillingappservices/demo/controller/sales/NewSaleServiceController.java
+++ src/main/java/com/javabillingappservices/demo/controller/sales/NewSaleServiceController.java
@@ -1,9 +1,7 @@
 package com.javabillingappservices.demo.controller.sales;
 
 import com.javabillingappservices.demo.model.sales.NewSaleModel;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/newsales")
@@ -13,5 +11,11 @@ public class NewSaleServiceController {
     public NewSaleModel getNewSalesResponse(String status){
         return new NewSaleModel("fadsf", "sadfafd", "dafsdf", 12);
     }
+
+    @PostMapping
+    public String postNewSalesRequest(@RequestBody NewSaleModel newSaleModel){
+        return "Inserted new sale";
+    }
+
 
 }
